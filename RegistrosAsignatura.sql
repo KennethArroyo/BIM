6df213064c72aed216065d82b3eca16bdda0c932
estmontero11 <30708446+estmontero11@users.noreply.github.com>
2018-09-19 08:51:31 -0600

    Delete RegistrosAsignatura.sql

--- RegistrosAsignatura.sql
+++
@@ -1,8 +0,0 @@
-use BIM 
-BEGIN TRAN 
-insert into Asignatura values('Historia')
-insert into Asignatura values('Geografia')
-insert into Asignatura values('Matematica')
-insert into Asignatura values('Computacion')
-commit 
-select * from Asignatura
